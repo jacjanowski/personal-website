9de760d381669929445e408b4a096055bc261cec
Jacob Janowski <60745870+jacjanowski@users.noreply.github.com>
2022-03-27 13:15:36 -0500

    change images and add links. resize header

--- src/App.js
+++ src/App.js
@@ -28,10 +28,9 @@ let brandImages = [
 ];
 
 const slider = (
-  
   <AwesomeSlider mobileTouch={true} bullets={false}>
     <div className="section">
-      <Fade top duration={3000}>
+      <Fade top duration={1500}>
         <Row>
           <h1>
             {" "}
@@ -47,28 +46,22 @@ const slider = (
           </p>
 
           <div id="brands">
-            <Fade bottom cascade duration={1000}>
+            <Fade delay={1000} bottom cascade duration={1000}>
               <img src={brandImages[0]} />
               <img src={brandImages[1]} />
               <img src={brandImages[2]} />
               <img src={brandImages[3]} />
-            </Fade>
 
-            <Fade bottom cascade duration={2500}>
               <img src={brandImages[4]} />
               <img src={brandImages[5]} />
               <img src={brandImages[6]} />
               <img src={brandImages[7]} />
-            </Fade>
 
-            <Fade bottom cascade duration={4000}>
               <img src={brandImages[8]} />
               <img src={brandImages[9]} />
               <img src={brandImages[10]} />
               <img src={brandImages[11]} />
-            </Fade>
 
-            <Fade bottom cascade duration={5500}>
               <img src={brandImages[12]} />
               <img src={brandImages[13]} />
               <img src={brandImages[14]} />
@@ -100,11 +93,9 @@ const slider = (
                 <span className="highlight">NodeJS & Express</span> to pull
                 Historical data.
               </p>
-              <a href="#">View more</a>
+              <a target={"_blank"} href="https://historytimeline.herokuapp.com/">View more</a>
             </figcaption>
           </figure>
-
-          
         </div>
 
         <div className="grid">
@@ -122,14 +113,14 @@ const slider = (
                 SPA created using <span className="highlight">React</span> to
                 showcase the NFT on the Solana Blockchain.
               </p>
-              <a href="#">View more</a>
+              <a target={"_blank"} href="https://daredu.io/">View more</a>
             </figcaption>
           </figure>
         </div>
 
         <div className="grid">
           <figure className="effect-marley">
-            <img src="images/candy-crush.png" alt="img11" />
+            <img src="images/candy-crush.PNG" alt="img11" />
             <figcaption>
               <h2>
                 Candy Crush <span>Game</span>
@@ -139,7 +130,7 @@ const slider = (
                 recreate the Candy Crush game with use of{" "}
                 <span className="highlight">Bootstrap & jQuery.</span>{" "}
               </p>
-              <a href="#">View more</a>
+              <a target={"_blank"} href="https://github.com/jacjanowski/candy-crush">View more</a>
             </figcaption>
           </figure>
         </div>
